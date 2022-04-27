6515984b105da603e5d991630342db7681ea3c71
Christian Beadling <beadlich@rose-hulman.edu>
2022-04-27 11:08:23 -0400

    init

---
+++ public/scripts/main.js
@@ -0,0 +1,108 @@
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * Christian Beadling and Matthew McClenahan
+ */
+
+var rhit = rhit || {};
+
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
+
+rhit.MovieQuote = class {
+	constructor(id, text) {
+		this.id = id;
+		this.text = text;
+	}
+}
+
+rhit.FbTextManager = class {
+
+}
+
+rhit.DuckyPageController = class {
+
+}
+
+
+
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+	signIn() {
+		Rosefire.signIn("8e84034b-cfa4-4084-9e49-db5c5282b24b", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			// Next use the Rosefire token with Firebase auth.
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				if (error.code === 'auth/invalid-custom-token') {
+					console.log("The token you provided is not valid.");
+				} else {
+					console.log("signInWithCustomToken error", error.message);
+				}
+			});
+		});
+
+
+	}
+	signOut() {
+		firebase.auth().signOut();
+	}
+	get uid() {
+		return this._user.uid;
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+}
+
+rhit.LoginPageController = class {
+	constructor() {
+		document.querySelector("#rosefireButton").onclick = (event) => {
+			rhit.fbAuthManager.signIn();
+		};
+	}
+}
+
+rhit.checkForRedirects = function () {
+	// Redirects
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/list.html";
+	}
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+}
+
+rhit.initializePage = function () {
+
+}
+
+rhit.main = function () {
+	console.log("Ready");
+	rhit.fbAuthManager = new rhit.FbAuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+		console.log(`The auth state has changed.   isSignedIn = ${rhit.fbAuthManager.isSignedIn}`);
+		rhit.checkForRedirects();
+		rhit.initializePage();
+	});
+};
+
+rhit.main();
